ad9ed712030d85726d8e716629d237f14170ae2d
VasyaPupkin120 <VPupkinza@yandex.ru>
2024-03-01 15:39:45 +0700

    генерация ссылок на zkb

--- config/settings.py
+++ config/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.cache.UpdateCacheMiddleware',
+    # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,7 +77,7 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 
-    'django.middleware.cache.FetchFromCacheMiddleware',
+    # 'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
--- dbeve_universe/views.py
+++ dbeve_universe/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
+from .models import Systems, Constellations, Regions
 
 def universe(request):
     return render(request, "dbeve_universe/universe.html")
 
 # Create your views here.
 def systems_list(request):
-    # if not Systems.objects.all():
-    #     add_systems_id_in_DB()
-    #     systems = Systems.objects.all()
-    #     return render(request, "dbeve/systems_id.html", context={"systems": systems, "status": "выполнена загрузка данных из json-файла в БД"})
-    # else:
-        # systems = Systems.objects.all()
-        # return render(request, "dbeve/systems_id.html", context={"systems": systems, "status": "Данные загружены из БД"})
-    return render(request, "dbeve_universe/systems.html")
+    if not Systems.objects.all():
+        return render(request, "dbeve_universe/systems.html", context={"status": "отсутствуют данные в БД"})
+    else:
+        systems = Systems.objects.all()
+        return render(request, "dbeve_universe/systems.html", context={"systems": systems, "status": "Данные загружены из БД"})
 
 def constellations_list(request):
-    return render(request, "dbeve_universe/constellations.html")
+    if not Constellations.objects.all():
+        return render(request, "dbeve_universe/constellations.html", context={"status": "отсутствуют данные в БД"})
+    else:
+        systems =  Constellations.objects.all()
+        return render(request, "dbeve_universe/constellations.html", context={"constellations": systems, "status": "Данные загружены из БД"})
 
 def regions_list(request):
-    return render(request, "dbeve_universe/regions.html")
+    if not Regions.objects.all():
+        return render(request, "dbeve_universe/regions.html", context={"status": "отсутствуют данные в БД"})
+    else:
+        systems =  Regions.objects.all()
+        return render(request, "dbeve_universe/regions.html", context={"regions": systems, "status": "Данные загружены из БД"})
