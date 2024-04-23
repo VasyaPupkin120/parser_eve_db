d5536442c67378f1ab31277b7fea0b059fd93992
VasyaPupkin120 <VPupkinza@yandex.ru>
2024-04-23 14:12:47 +0700

    дополнил модель киллмыла данными с зкб - цена и другие поля

--- dbeve_social/models.py
+++ dbeve_social/models.py
@@ -67,10 +67,10 @@ class VictimItems(models.Model):
     Для описания итемов, выпавших с пострадавшего.
     Нет нужды в поле response_body и dt_change.
     """
-    flag = models.BigIntegerField()
+    flag = models.BigIntegerField(null=True)
     # итем может быть унижчтожен или дропнут. И то и то должно быть в каком то количестве
-    status = models.CharField(choices=(("destroyed", "Итем уничтожен"), ("dropped", "Итем выпал")))
-    quantity = models.BigIntegerField()
+    status = models.CharField(choices=(("destroyed", "Итем уничтожен"), ("dropped", "Итем выпал")),null=True)
+    quantity = models.BigIntegerField(null=True)
 
     item_type = models.ForeignKey(Items, on_delete=models.SET_NULL, null=True)
     victim = models.ForeignKey("Victim", on_delete=models.CASCADE, related_name="items")
@@ -80,7 +80,7 @@ class Victim(models.Model):
     Вспомогательная модель для киллмыла - пострадавший. 
     Нет нужды в поле response_body и dt_change.
     """
-    damage_taken = models.BigIntegerField()
+    damage_taken = models.BigIntegerField(null=True)
 
     alliance = models.OneToOneField("Alliances", on_delete=models.SET_NULL, null=True)
     character = models.OneToOneField("Characters", on_delete=models.CASCADE, null=True)
@@ -91,9 +91,9 @@ class Attackers(models.Model):
     """
     Вспомогательная модель для киллмыла - запись об одном атакующем.
     """
-    damage_done = models.BigIntegerField()
-    final_blow = models.BooleanField()
-    security_status = models.FloatField()
+    damage_done = models.BigIntegerField(null=True)
+    final_blow = models.BooleanField(null=True)
+    security_status = models.FloatField(null=True)
 
     alliance = models.OneToOneField("Alliances", on_delete=models.SET_NULL, null=True)
     character = models.OneToOneField("Characters", on_delete=models.CASCADE, null=True)
@@ -104,13 +104,25 @@ class Attackers(models.Model):
 
 class Killmails(BaseEntity):
     """
-    Класс одиночного киллмыла
+    Модель одиночного киллмыла.
     """
+    # поля из esi-запроса
     killmail_id = models.BigIntegerField(primary_key=True)
-    killmail_time = models.DateTimeField()
-    position_x = models.DecimalField(max_digits=32, decimal_places=0)
-    position_y = models.DecimalField(max_digits=32, decimal_places=0)
-    position_z = models.DecimalField(max_digits=32, decimal_places=0)
+    killmail_time = models.DateTimeField(null=True)
+    position_x = models.DecimalField(max_digits=32, decimal_places=0, null=True)
+    position_y = models.DecimalField(max_digits=32, decimal_places=0, null=True)
+    position_z = models.DecimalField(max_digits=32, decimal_places=0, null=True)
+    # поля из zkb-запроса
+    locationID = models.CharField(null=True)
+    killmail_hash = models.CharField(null=True)
+    fittedValue = models.DecimalField(max_digits=32, decimal_places=2, null=True)
+    droppedValue = models.DecimalField(max_digits=32, decimal_places=2, null=True)
+    destroyedValue = models.DecimalField(max_digits=32, decimal_places=2, null=True)
+    totalValue = models.DecimalField(max_digits=32, decimal_places=2, null=True)
+    points = models.BigIntegerField(null=True)
+    npc = models.BooleanField(null=True)
+    solo = models.BooleanField(null=True)
+    awox = models.BooleanField(null=True)
 
     solar_system = models.ForeignKey(Systems, on_delete=models.SET_NULL, null=True)
     ship_type = models.ForeignKey(ShipTypes, on_delete=models.SET_NULL, null=True)
