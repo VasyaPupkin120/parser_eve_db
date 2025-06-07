fabf1e4cea16bc578ce041d40b01197b26df505f
VasyaPupkin120 <VPupkinza@yandex.ru>
2025-06-07 10:38:13 +0700

    перенос логики копирования в файл js

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,17 @@
+// копирует текст из связанного блока
+function CopyTextContainer(containerid) {
+        let textarea = document.createElement('textarea');
+        textarea.id = 'temp';
+        textarea.style.height = 0;
+        document.body.appendChild(textarea);
+        textarea.value = document.getElementById(containerid).innerText;
+        let selector = document.querySelector('#temp');
+        selector.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+}
+
+
 // на основе отметок типов шипов, алли, корп выставляет галочки на киллмылах
 function UpdateKillmails()
 {
